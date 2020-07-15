f8967e1bf160313049569bf2a7f2bdce743705d8
Ramesh <ramesharavindhan96@gmail.com>
2020-07-15 15:55:57 +0530

    new commit july 15

--- src/test/java/com/feature/TestRunner.java
+++ src/test/java/com/feature/TestRunner.java
@@ -10,13 +10,8 @@ import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 
 import org.apache.commons.io.FileUtils;
-import static org.junit.Assert.*;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.junit.Test;
 
-import com.intuit.karate.KarateOptions;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
 
@@ -35,7 +30,7 @@ public class TestRunner {
 	        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);        
 	    }
 	   
-	   public static void generateReport(String karateOutputPath) {        
+	 public static void generateReport(String karateOutputPath) {        
 	        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
 	        List<String> jsonPaths = new ArrayList<String>(jsonFiles.size());
 	        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
